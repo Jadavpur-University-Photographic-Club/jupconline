49ba331977782735102023ec60f460c8216d090e
JUPC <138682808+jupconline@users.noreply.github.com>
2024-02-15 11:23:05 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,6 @@
 const countdown = () => {
-    const countDate = new Date().getTime();
-    // const countDate = new Date("June 5, 2024 00:00:00").getTime();
+    // const countDate = new Date().getTime();
+    const countDate = new Date("May 22, 2024 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
